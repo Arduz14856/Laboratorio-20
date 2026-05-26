5e0f05db16e0c101385a6bd3c5f3072fbbe68945
Arduz <arduzaliendro@gmail.com>
2026-05-25 21:04:47 -0400

    Corregir middleware no cache

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
 
-    $middleware->append(\App\Http\Middleware\CheckRole::class);
+    $middleware->append(\App\Http\Middleware\NoCache::class);
 
     $middleware->alias([
         'role' => \App\Http\Middleware\CheckRole::class,
